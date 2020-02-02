4127667f77c1a0058a6b082c5925d0f218b2bd55
aditirao7 <aditirao725@gmail.com>
2020-02-02 19:24:40 +0530

    /

--- AI/sockets/2camserver.py
+++ AI/sockets/2camserver.py
@@ -1,32 +1,40 @@
-import socket
 import cv2
-import numpy as np
-import pickle
+import threading
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 8000))
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('', 7000))
-sock.listen(5)
-s.listen(5)
-clt1,adr1 = sock.accept()
-clt,adr = s.accept()
-cap = cv2.VideoCapture(4)
-cap1= cv2.VideoCapture(0)
-while (1):
-    ret, frame = cap.read()
-    ret1, frame1 = cap1.read()
-    #size = pickle.dumps(np.size(frame))
-    frame1 = pickle.dumps(frame1)
-    frame= pickle.dumps(frame)
-    #clt.sendall(size)
-    answer=clt.recv(8)
-    clt1.sendall(frame1)
-    clt.sendall(frame)
+def cam1():
+    cap = cv2.VideoCapture(4)
+    while 1:
+        ret, frame = cap.read()
+        cv2.imshow("cam1", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    return 0
 
-cap.release()
-cap1.release()
-clt.close()
-s.close()
-clt1.close()
-sock.close()
+def cam2():
+    cap1 = cv2.VideoCapture(0)
+    while 1:
+        ret1, frame1 = cap1.read()
+        cv2.imshow("cam2", frame1)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cap1.release()
+    cv2.destroyAllWindows()
+    return 0
+
+while 1:
+    if __name__ == "__main__":
+
+        t1 = threading.Thread(target=cam1, name='cam1')
+        t2 = threading.Thread(target=cam2, name='cam2')
+
+        t1.start()
+        t2.start()
+
+        if t1.isAlive()==True:
+            t1.kill()
+        if t2.isAlive()==True:
+            t2
+
+cv2.destroyAllWindows()
--- servercompress.py
+++ servercompress.py
@@ -2,20 +2,27 @@ import socket
 import cv2
 import numpy as np
 import pickle
+import time
 
 s = socket.socket()
-s.bind(('', 8000))
+s.bind(('', 7000))
 s.listen(3)
 clt, adr= s.accept()
 
 cap = cv2.VideoCapture(0)
+
+cap.set(3,200)
+cap.set(4,150)
+time.sleep(5)
+
 while 1:
     _, frame = cap.read()
-    frame = cv2.imencode('.jpeg', frame)
-    print(np.size(frame[1]))
+    frame = cv2.imencode('.jpg', frame)
     frame_size = pickle.dumps(np.size(frame[1]))
+    print(np.size(frame[1]))
     frame = pickle.dumps(frame)
     clt.sendall(frame_size)
+    time.sleep(0.01)
     answer = clt.recv(8)
     clt.sendall(frame)
 
