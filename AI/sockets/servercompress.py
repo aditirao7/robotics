d696d8f5caf6e46bb53c4549fe971fb8420e0f9c
aditirao7 <57457307+aditirao7@users.noreply.github.com>
2020-02-10 17:53:27 +0530

    Update servercompress.py

--- AI/sockets/servercompress.py
+++ AI/sockets/servercompress.py
@@ -4,28 +4,22 @@ import numpy as np
 import pickle
 import time
 
-s = socket.socket()
-s.bind(('', 7000))
-s.listen(3)
-clt, adr= s.accept()
-
 cap = cv2.VideoCapture(0)
+cap.set(3, 200)
+cap.set(4, 150)
 
-cap.set(3,200)
-cap.set(4,150)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 8000))
+s.listen(3)
+clt, adr = s.accept()
+
 time.sleep(5)
-
-while 1:
-    _, frame = cap.read()
+while (True):
+    ret, frame = cap.read()
+    clt.send(pickle.dumps("b1"))
     frame = cv2.imencode('.jpg', frame)
-    frame_size = pickle.dumps(np.size(frame[1]))
-    print(np.size(frame[1]))
     frame = pickle.dumps(frame)
-    clt.sendall(frame_size)
-    time.sleep(0.01)
-    answer = clt.recv(8)
     clt.sendall(frame)
 
 clt.close()
-s.close()
 cap.release()
